a55850278b335c4a9d9886ed4a3fd740a4dde0c0
anoooj0 <130328624+anoooj0@users.noreply.github.com>
2023-11-29 23:25:56 +0000

    JS Solo Coding

--- createAppend.js
+++ createAppend.js
@@ -1,28 +1,30 @@
-// html elements
-let body = document.querySelector("body");
-let container = document.querySelector(".container");
+// // html elements
+// let body = document.querySelector("body");
+// let container = document.querySelector(".container");
 
-// 1. Create an h2 element.
-// - Store it in a variable called h2.
-
-
-// 2. Set the innerHTML of h2 to your name.
+// // 1. Create an h2 element.
+// // - Store it in a variable called h2.
+let h2 = document.createElement("h2");
 
 
-
-// 3. Select the container div and append the h2 element.
+// // 2. Set the innerHTML of h2 to your name.
+h2.innerHTML = "Anuj Shrestha";
 
 
 
-// 4. Create a paragraph element.
-// - Store it in a variable called para.
+// // 3. Select the container div and append the h2 element.
+container.appendChild(h2);
 
 
-// 5. Set the innerHTML of para to a fact about yourself.
+// // 4. Create a paragraph element.
+// // - Store it in a variable called para.
+para = document.createElement("p")
 
+// // 5. Set the innerHTML of para to a fact about yourself.
+para.innerHTML = "I am 6'0 tall"
 
-// 6. Select the container div and append the paragraph element.
-
+// // 6. Select the container div and append the paragraph element.
+container.appendChild(para);
 
 
 // Uncomment the code below after completing 1-6.
@@ -31,3 +33,7 @@ let container = document.querySelector(".container");
 // let h1 = document.createElement('h1');
 // h1.innerHTML = "My Webpage";
 // body.appendChild(h1);
+
+let h1 = document.createElement('h1')
+h1.innerHTML = "My Webpage";
+body.appendChild(h1);
--- jsLab.js
+++ jsLab.js
@@ -16,13 +16,26 @@ let tile9 = document.querySelector('.tile-9')
 // 1. Tile 1 feels a little sad. 
 // - Update the inner HTML to show an encouraging message! 
 // - Something like "YOU GOT THIS!" or even a "You're a beautiful person!" 
-
+tile1.innerHTML = "You Got This"
 
 
 // 2. Tile 2 has a button. 
 // - Create a click event and have the background color of tile 2 change to a shade of green.
 // - BE SURE TO SAVE THE BUTTON FIRST AS A VARIABLE.
 // - Can you see if there's a way to toggle it???
+clickButton = document.querySelector('button');
+
+let originalColor = tile2.style.backgroundColor
+
+clickButton.addEventListener("click", (event) => {
+  if (tile2.style.backgroundColor === "green") {
+    tile2.style.backgroundColor = originalColor;
+  }
+  else {
+    tile2.style.backgroundColor = "green"
+  }
+});
+
 
 
 // 3. What's this? A totoro? Where's his friends? 
@@ -30,11 +43,13 @@ let tile9 = document.querySelector('.tile-9')
 // - Once created, set the src of it to the image address.
 // - Append the element to tile 3. 
 // - Need some inspiration? Go to giphy.com, search a character and choose the "stickers".
-
+friendImage = document.createElement("img");
+friendImage.src = "https://media2.giphy.com/media/ZYZEFjLzOV3fq/giphy.gif?cid=ecf05e47jwlib5tvy4d1f0248ryigzn7en1qu2rs63nv9mjo&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+tile3.appendChild(friendImage);
 
 // 4. Oh no! A broken image! We need to fix this.
 // - Set the src for tile 4 to have an image of your favorite TV show.
-
+tile4.src = "https://tv-fanatic-res.cloudinary.com/iu/s--GAhBhEba--/t_slideshow/cs_srgb,f_auto,fl_strip_profile.lossy,q_auto:420/v1503671939/slides/2659/high-times.png"
 
 
 
@@ -46,17 +61,22 @@ let tile9 = document.querySelector('.tile-9')
 
 // 5. Hmm. Looks like the font is too small. 
 // - Update the font-size of tile 5 to 35 pixels.
-
+tile5.style.fontSize = "35px"
 
 // 6. Another sad box! 
 // - Let make this tile 6 feel special and add a gradient background! 
 // - What kind of gradient? What color? YOU CHOOSE!
 
+tile6.style.backgroundColor = ""
+
 // 7. Too many squares. 
 // - Let's change tile 7 to have rounded edges - be sure to search for which CSS property does this :)
+tile7.style.borderRadius = "20px"
 
 // 8. Replace the image with your favorite emoji. 
 // - Try going to this site to search: https://emojipedia.org/
+
+tile8.src = "https://m.media-amazon.com/images/I/417ayPE62RL._UF1000,1000_QL80_.jpg"
 
 // 9. Display a random number in the tile when the button is clicked.
 // - BE SURE TO SAVE THE BUTTON FIRST AS A VARIABLE.
